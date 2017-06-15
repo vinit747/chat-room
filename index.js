f208d9b162ee9d81d502e1a7a604e729d43dc3f8
Vinit <vinit_asher@yahoo.com>
2017-06-16 04:24:08 +0530

    Final commit
    Some minor problem in array of user when the user refreshes the page (disconnect and reconnect).

---
+++ index.js
@@ -0,0 +1,69 @@
+var express =require("express");
+var app=express();
+var cookieparser=require("cookie-parser");
+var morgan=require("morgan");
+var port = process.env.PORT || 3000;
+var $=require('jquery');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var session=require("express-session");
+var bodyparser=require('body-parser');
+var urlencodedParser = bodyparser.urlencoded({ extended: false });
+var user;
+var ct=0;
+var onl=[];
+
+
+app.use(bodyparser.json());
+app.use(morgan("dev"));
+app.use(express.static(__dirname ));
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(cookieparser());
+app.set('views', __dirname + '/views');
+app.set('view engine','ejs');
+
+
+
+app.get('/',function(req,res){
+	res.render("p1");
+});
+
+app.get('/room',function(req,res){
+  //console.log(req.query.name);
+  user=req.query.name;
+	res.render("p2",{us:req.query.name});
+})
+
+io.on('connection', function(socket){
+
+  socket.on("entered",function(x){
+    onl[ct]=x;
+    ct++;
+    io.emit("broad",x);
+    io.emit("online",onl);
+  });
+  socket.on("typing",function(who){
+    io.emit("ty",who);
+  });
+  socket.on("usr",function(nm){
+     user=nm;
+  });
+  socket.on('msgs',function(m){
+    io.emit("typer",user);
+    io.emit("msgs",m);
+  }); 
+  socket.on('disconnect', function(){
+    var str=(socket.handshake.headers.referer).substr(32);
+    var pos=onl.indexOf(str);
+    onl.splice(pos,1);
+    io.emit("nbroad",str);
+    io.emit("online",onl);
+    if (onl.length==0)
+      ct=0;
+  });
+});
+
+
+http.listen(port,function(){
+  console.log("Started on "+port);
+});
